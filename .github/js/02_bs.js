b4e2369e0a03af07c0be41a58e408539216df068
Aditya chaudhary <a83185898065@gmail.com>
2026-06-10 06:42:14 +0000

    setup

---
+++ .github/js/02_bs.js
@@ -0,0 +1,15 @@
+const accountId = 1445566
+let accountEmail = "Aditya@1234"
+var accountPassword = "12345"
+accountcity = "jaipur"
+
+
+//accountId = 2 // not allowed
+
+accountEmail ="hc@gmail.com"
+accountPassword ="123456"
+accountcity = "Bengaluru"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountcity])
