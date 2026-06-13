68c1c2403762947719cf7e18272adf5299acdc7f
Aditya chaudhary <a83185898065@gmail.com>
2026-06-13 01:53:03 +0000

    04_bs.js

---
+++ .github/js/04_bs.js
@@ -0,0 +1,28 @@
+//let score = "33abc"
+//let score = "null"
+
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; flase =>0
+
+let bolleanIsloggdIn = Boolean(isLoggedIn)
+console.log(bolleanIsloggdIn);
+
+// 1 => true;0 => false 
+// "" => false 
+// "Aditya" => true
+
+let someNumber = 33
+
+
+let stringNumber = string(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
