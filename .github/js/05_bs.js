2ab085e09f7b510743781df579b09a0e060558a0
Aditya chaudhary <a83185898065@gmail.com>
2026-06-13 06:07:41 +0000

    05_bs.js

---
+++ .github/js/05_bs.js
@@ -0,0 +1,35 @@
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2*3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let star1 = "hello"
+// let star2 = "Aditya"
+// sum = 0
+// sum = star1 + star2
+// console.log(sum);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2 );
+// console.log((3 + 4) * 5 % 3);
+
+console.lo(+true)
+console.log(+"")
+
+num1 = num2 = num3 = 2+2
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
+
+
+
